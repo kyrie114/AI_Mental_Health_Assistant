c3afc5256f4044766e176a940c804d110e705ddc
陆顺鑫 <2018869035@qq.com>
2026-03-01 22:05:23 +0800

    路由配置以及定义路径别名如@

--- ai-vue/vite.config.js
+++ ai-vue/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import resolve from 'path'//用于处理路径
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),//别名配置定义了@别名指向src目录，方便在代码中引用src目录下的文件
+    },
+  },
 })
